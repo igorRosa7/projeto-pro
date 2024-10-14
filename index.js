e3f1b6958da596ea5a25d41be76c03589fba7865
igorSoares7 <igorsoaresr_contato@hotmail.com>
2024-10-14 07:13:56 -0300

    Adiciona overflow na tabela

--- index.js
+++ index.js
@@ -6,17 +6,14 @@ window.addEventListener("load", iniciarProcesso)
 iniciaEvento("input-busca", "keyup", iniciarBusca)
 
 let livros = []
-console.log("contexto global!")
 
 async function iniciarProcesso(){
-    console.log("inciando processo!")
     livros = await chamadaGET(URL_API_LIVROS)
     construirTabelaComLivros(livros)
     
 }
 
 function iniciarBusca(){
-    console.log("iniciarbusca")
     const valorParaBuscar = document.getElementById("input-busca").value
     const livrosFiltrados = livros.filter((livro => livro.title.includes(valorParaBuscar)))
     construirTabelaComLivros(livrosFiltrados)
